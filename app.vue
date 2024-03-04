badf20570e73321056554ec81cefca25c1a075e2
wronah <hubertwro7@gmail.com>
2024-03-04 23:47:46 +0100

    added header with button which toggles dark and light mode

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
+<script setup lang="ts">
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <Header />
   </div>
 </template>
---
+++ components/Header.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+function toggleDarkMode() {
+    const body = document.querySelector('body')
+    body?.classList.toggle('dark')
+}
+
+</script>
+<template>
+    <div class="flex flex-row justify-end p-3">
+        <Button @click="toggleDarkMode()"></Button>
+    </div>
+</template>
