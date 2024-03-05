2a33197878dbf81515d55b7ed5aa6afc5cf38b9d
wronah <hubertwro7@gmail.com>
2024-03-05 23:52:53 +0100

    added nuxt lucide icons

--- components/Header.vue
+++ components/Header.vue
@@ -1,13 +1,28 @@
 <script setup lang="ts">
+import { Sun, Moon } from 'lucide-vue-next'
+
+const isDark = ref(false)
 
 function toggleDarkMode() {
     const body = document.querySelector('body')
-    body?.classList.toggle('dark')
+    isDark.value = body!.classList.contains('dark')
+
+    if(isDark.value) {
+        body!.classList.remove('dark')
+        isDark.value = false
+    }
+    else {
+        body!.classList.add('dark')
+        isDark.value = true
+    }
 }
 
 </script>
 <template>
     <div class="flex flex-row justify-end p-3">
-        <Button @click="toggleDarkMode()"></Button>
+        <Button @click="toggleDarkMode()">
+            <Sun v-if="isDark"/>
+            <Moon v-else />
+        </Button>
     </div>
 </template>
