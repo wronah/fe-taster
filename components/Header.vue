ba819309a7bbb183fca03f4de643ee393ee49a4b
wronah <hubertwro7@gmail.com>
2024-04-10 19:31:50 +0200

    add basic header layout, add avatar component to project

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,15 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 <template>
-    <header class="flex flex-row justify-end p-2" >
+  <header class="sticky z-40 top-0 bg-background/80 backdrop-blur-lg border-b border-border">
+    <div class="container flex justify-between items-center max-w-screen-2xl h-12">
+      <div>
+        <a href="/">
+         <span class="font-bold">taster</span>
+        </a>
+      </div>
+      <div class="flex items-center">
         <ThemeSwitcher />
-    </header>
+      </div>
+    </div>
+  </header>
 </template>
--- components/ThemeSwitcher.vue
+++ components/ThemeSwitcher.vue
@@ -4,10 +4,10 @@ const colorMode = useColorMode()
 </script>
 
 <template>
-    <ColorScheme tag="theme-switcher">
-        <Button @click="() => colorMode.preference === 'dark' ? colorMode.preference = 'light' : colorMode.preference = 'dark'">
-            <Sun v-if="colorMode.preference === 'dark'"/>
-            <Moon v-else />
-        </Button>
-    </ColorScheme>
+  <ColorScheme tag="theme-switcher">
+    <Button variant="outline" class="w-8 h-8 border-none" size="icon" @click="() => colorMode.preference === 'dark' ? colorMode.preference = 'light' : colorMode.preference = 'dark'">
+      <Sun class="w-4 h-4" v-if="colorMode.preference === 'dark'"/>
+      <Moon class="w-4 h-4" v-else />
+    </Button>
+  </ColorScheme>
 </template>
